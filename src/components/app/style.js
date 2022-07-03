2147835d380cf0b0a4035044f9f856f72d2fb1e1
Yana Rozova <yana.rozova@mail.ru>
2022-07-03 22:09:12 +0300

    fix fonts

--- src/components/app/style.js
+++ src/components/app/style.js
@@ -1,21 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: "Inter";
-    src: url("https://fonts.googleapis.com/css2?family=Inter:wght@400";
-    font-weight: 400;
-    font-style: normal;
-    font-display: swap;
-  }
-
-  @font-face {
-    font-family: "Inter";
-    src: url("https://fonts.googleapis.com/css2?family=Inter:wght@700";
-    font-weight: 700;
-    font-style: normal;
-    font-display: swap;
-  }
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
 
   html {
     width: 100%;
